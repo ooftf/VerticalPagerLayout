ffb2beca827f1bed2d10115434b47b8462009c7c
99474 <994749769@qq.com>
2017-12-26 14:19:06 +0800

    完善代码

---
+++ app/src/main/java/com/ooftf/verticalviewpager/VerticalViewPagerAdapter.kt
@@ -0,0 +1,44 @@
+package com.ooftf.verticalviewpager
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+/**
+ * Created by 99474 on 2017/12/26 0026.
+ */
+class VerticalViewPagerAdapter(var context:Context) : PagerAdapter() {
+    private var inflater:LayoutInflater = LayoutInflater.from(context)
+    override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+        return  view == `object`
+    }
+
+    override fun getCount(): Int {
+        return 5
+    }
+
+    override fun instantiateItem(container: ViewGroup?, position: Int): Any {
+        var view =inflater.inflate(R.layout.page_scroll,container,false)
+        var child = view.findViewById<View>(R.id.view)
+        var button = view.findViewById<View>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(context,"$position",Toast.LENGTH_SHORT).show()
+        }
+        when(position){
+            0->child.setBackgroundColor(Color.parseColor("#00FFFF"))
+            1->child.setBackgroundColor(Color.parseColor("#FF00FF"))
+            2->child.setBackgroundColor(Color.parseColor("#FFFF00"))
+            3->child.setBackgroundColor(Color.parseColor("#0000FF"))
+            else->child.setBackgroundColor(Color.parseColor("#FF0000"))
+        }
+        container?.addView(view)
+        return view
+    }
+    override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+        container?.removeView(`object` as View?);
+    }
+}
