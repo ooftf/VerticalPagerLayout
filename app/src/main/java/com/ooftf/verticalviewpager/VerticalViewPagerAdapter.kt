907b1afac4729e56461904eae09e746cc9025798
99474 <994749769@qq.com>
2017-12-26 18:44:48 +0800

    adapter添加复用机制

--- app/src/main/java/com/ooftf/verticalviewpager/VerticalViewPagerAdapter.kt
+++ app/src/main/java/com/ooftf/verticalviewpager/VerticalViewPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.ooftf.verticalviewpager
 import android.content.Context
 import android.graphics.Color
 import android.support.v4.view.PagerAdapter
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,7 @@ import android.widget.Toast
  */
 class VerticalViewPagerAdapter(var context:Context) : PagerAdapter() {
     private var inflater:LayoutInflater = LayoutInflater.from(context)
+    private var cach:MutableMap<Int,View> = HashMap()
     override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
         return  view == `object`
     }
@@ -22,23 +24,33 @@ class VerticalViewPagerAdapter(var context:Context) : PagerAdapter() {
     }
 
     override fun instantiateItem(container: ViewGroup?, position: Int): Any {
-        var view =inflater.inflate(R.layout.page_scroll,container,false)
-        var child = view.findViewById<View>(R.id.view)
-        var button = view.findViewById<View>(R.id.button)
-        button.setOnClickListener {
-            Toast.makeText(context,"$position",Toast.LENGTH_SHORT).show()
+        val recycler = cach[position]
+        val view = if (recycler == null){
+            val view = inflater.inflate(R.layout.page_scroll,container,false)
+            val child = view.findViewById<View>(R.id.view)
+            val button = view.findViewById<View>(R.id.button)
+            button.setOnClickListener {
+                Toast.makeText(context,"$position",Toast.LENGTH_SHORT).show()
+            }
+            when(position){
+                0->child.setBackgroundColor(Color.parseColor("#00FFFF"))
+                1->child.setBackgroundColor(Color.parseColor("#FF00FF"))
+                2->child.setBackgroundColor(Color.parseColor("#FFFF00"))
+                3->child.setBackgroundColor(Color.parseColor("#0000FF"))
+                else->child.setBackgroundColor(Color.parseColor("#FF0000"))
+            }
+            view
+        }else{
+            Log.e("instantiateItem","复用")
+            cach.remove(position)
+            recycler
         }
-        when(position){
-            0->child.setBackgroundColor(Color.parseColor("#00FFFF"))
-            1->child.setBackgroundColor(Color.parseColor("#FF00FF"))
-            2->child.setBackgroundColor(Color.parseColor("#FFFF00"))
-            3->child.setBackgroundColor(Color.parseColor("#0000FF"))
-            else->child.setBackgroundColor(Color.parseColor("#FF0000"))
-        }
+
         container?.addView(view)
         return view
     }
     override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
         container?.removeView(`object` as View?);
+        cach.put(position, `object` as View)
     }
 }
