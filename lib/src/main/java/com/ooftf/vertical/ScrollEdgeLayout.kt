5c63b8437835a427070a775ac9492006a1c7df4b
99474 <994749769@qq.com>
2017-12-29 15:47:23 +0800

    修复下边界计算错误问题

--- lib/src/main/java/com/ooftf/vertical/ScrollEdgeLayout.kt
+++ lib/src/main/java/com/ooftf/vertical/ScrollEdgeLayout.kt
@@ -60,11 +60,13 @@ class ScrollEdgeLayout : FrameLayout, EdgeWrapper {
             scrollingView.computeVerticalScrollExtent()+scrollingView.computeVerticalScrollOffset() >= scrollingView.computeVerticalScrollRange()
         }else if (scrollView is WebView){
             var webView =  scrollView as WebView
-            Log.e(((webView.height+webView.scrollY)*1.001).toString(),(webView.contentHeight*webView.scale).toString())
+            if(BuildConfig.DEBUG){
+                Log.e(((webView.height+webView.scrollY)*1.001).toString(),(webView.contentHeight*webView.scale).toString())
+            }
             (webView.height+webView.scrollY)*1.001 >=webView.contentHeight*webView.scale//1.001作用 :有些网站会有误差，具体原因不知，代表例子https://github.com/marketplace  会有大约5的误差
 
         }else {
-            scrollView.scrollY + height >= scrollView.getChildAt(0).height
+            scrollView.scrollY + scrollView.height >= scrollView.getChildAt(0).height
         }
     }
 }
